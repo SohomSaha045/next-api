98591359acc0273fc89ba9fed59ed1d90c8a6cb0
Slutishboy <sohomsaha361@gmail.com>
2023-10-10 02:03:27 +0530

    POST request

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,7 +1,23 @@
 import { user } from "@/util/db";
 import { NextResponse } from "next/server";
 
-export async function GET(request){
-    const data=user;
-    return NextResponse.json(data,{status:200})
+export async function GET(request) {
+  const data = user;
+  return NextResponse.json(data, { status: 200 });
+}
+
+export async function POST(request) {
+  let payload = await request.json();
+  if (!payload.name || !payload.age || !payload.email) {
+    return NextResponse.json({ status: "no payload" }, { status: 501 });
+  }
+  return NextResponse.json(
+    {
+      Name: payload.name,
+      Age: payload.age,
+      Email: payload.email,
+      Status: "User Craeted",
+    },
+    { status: 201 }
+  );
 }
