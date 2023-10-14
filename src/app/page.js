4189c547df97419687d0c521748cc07b56fda262
Slutishboy <sohomsaha361@gmail.com>
2023-10-15 02:30:44 +0530

    routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,27 @@
-import Image from 'next/image'
-import styles from '../app/style.css'
-
+"use client";
+import styles from "../app/style.css";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const gotADD = () => {
+    router.push("/addUser");
+  };
+  const gotList = () => {
+    router.push("/users");
+  };
   return (
-    <div>
-    <h1>
-    Home Page
-    </h1>
+    <div style={{width:"100%",backgroundColor:"grey"}}>
+        <div >
+        <button type="button" onClick={gotADD} className="btn">
+        Add User to Database
+        </button>
+        <button type="button" onClick={gotList} className="btn">
+         Go to User List
+        </button>
+        </div>
+
+      
     </div>
-  )
+  );
 }
