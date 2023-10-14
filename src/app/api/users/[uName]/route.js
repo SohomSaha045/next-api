4002b9c6b29107c7073343285418e1459bf6b4d5
Slutishboy <sohomsaha361@gmail.com>
2023-10-15 02:08:48 +0530

    Dynamic-get-request-working

--- src/app/api/users/[uName]/route.js
+++ src/app/api/users/[uName]/route.js
@@ -1,39 +1,71 @@
+import { User } from "@/app/lib/model/user";
 import { user } from "@/util/db";
 import { NextResponse } from "next/server";
+import { cUrl } from "../../../lib/connect";
+import mongoose from "mongoose";
 
-export async function GET(req,content){
-    // content gets the parameters 
-    // console.log(content);
 
-    const data=user;
-    const res=data.filter((item)=>item.name==content.params.uName)
 
-    return NextResponse.json(res.length!=0?res:{status:"Data not found"},res.length!=0?{status:200}:{status:404})
-}
-export async function PUT(request,content){
-    let payload= await request.json();
-    let userName=content.params.uName;
-    if ( payload.name!=userName || !payload.name || !payload.age || !payload.email) {
-        return NextResponse.json({ status: "In Correct payload" }, { status: 400 });
+export async function GET(req, content) {
+  // content gets the parameters
+  // console.log(content);
+  try {
+    await mongoose.connect(cUrl);
+    let data = await User.find({ name: content.params.uName });
+    // console.log(data);
+    return NextResponse.json(
+      
+        data
+      ,
+      {
+        status: 200,
       }
-    // console.log(userName); 
-    return NextResponse.json({"result":payload,"status":"Success"},{status:200});
+    );
+    // const data = user;
+    // const res = data.filter((item) => item.name == content.params.uName);
+  } catch (e) {
+    return NextResponse.json(
+      { status: "Data not found", error: e },
+      { status: 404 }
+    );
+  }
 }
-export function DELETE(request,content){
-    let userName=content.params.uName;
-    if(userName){
-        return NextResponse.json({
-            result:"User Deleted"
-        },{
-            status:200
-        })
-    }
-    else{
-        return NextResponse.json({
-            result:"Internal Error Please after Some Time"
-        },{
-            status:400
-        })
-    }
-
+export async function PUT(request, content) {
+  let payload = await request.json();
+  let userName = content.params.uName;
+  if (
+    payload.name != userName ||
+    !payload.name ||
+    !payload.age ||
+    !payload.email
+  ) {
+    return NextResponse.json({ status: "In Correct payload" }, { status: 400 });
+  }
+  // console.log(userName);
+  return NextResponse.json(
+    { result: payload, status: "Success" },
+    { status: 200 }
+  );
+}
+export function DELETE(request, content) {
+  let userName = content.params.uName;
+  if (userName) {
+    return NextResponse.json(
+      {
+        result: "User Deleted",
+      },
+      {
+        status: 200,
+      }
+    );
+  } else {
+    return NextResponse.json(
+      {
+        result: "Internal Error Please after Some Time",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
 }
--- src/app/users/[name]/page.js
+++ src/app/users/[name]/page.js
@@ -1,28 +1,24 @@
 const getUsers = async (name) => {
-    let data = await fetch(`http://localhost:3000/api/users/${name}`);
-    data = await data.json();
-    // data= await data.filter((e)=>e.name==name);
-    return data;
-};
-export default async function Login({params}){
-    const data= await getUsers(params.name);
-     return (
+  let data = await fetch(`http://localhost:3000/api/users/${name}`);
 
-        <div>
-         {
-            data.map(
-                (e)=>{
-                    return (
-                        <>
-                        <h1>Name: {e.name}</h1>
-                        <h2>Age: {e.age}</h2>
-                        <h3>Email: {e.email}</h3>
-                        </>
-                    )
-                }
-            )
-         }
-        
-        </div>
-    )
+  data = await data.json();
+  // data= await data.filter((e)=>e.name==name);
+  return data;
+};
+export default async function Login({ params }) {
+  const data = await getUsers(params.name);
+  return (
+    <div>
+      {
+        data.map((e) => {
+        return (
+          <>
+            <h1>Name: {e.name}</h1>
+            <h2>Age: {e.age}</h2>
+            <h3>Email: {e.email}</h3>
+          </>
+        );
+      })}
+    </div>
+  );
 }
